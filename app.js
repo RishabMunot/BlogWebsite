fd12fb6a8e5264686443e924f179171403675295
RishabMunot <rishabmunot.az@gmail.com>
2020-01-25 17:15:42 +0530

    challange 20 completed

--- app.js
+++ app.js
@@ -3,8 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
-const posts = []
+const posts = [];
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -21,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.render("home", { content: homeStartingContent,posts:posts})
+  res.render("home", { content: homeStartingContent, posts: posts });
 });
 
 app.get("/about", function(req, res) {
@@ -37,18 +38,20 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-  
   const post = { postTitle: req.body.postTitle, postBody: req.body.postBody };
-  posts.push(post)
-  
-  res.redirect("/");
+  posts.push(post);
 
+  res.redirect("/");
 });
 
-
-app.get("/posts/:postName",function (req,res) {
-  console.log(req.params.postName); 
-})
+app.get("/posts/:postName", function(req, res) {
+  const postName = _.lowerCase(req.params.postName);
+  posts.forEach(function(post) {
+    if (_.lowerCase(post.postTitle) == postName) {
+      res.render("post", { postTitle: post.postTitle, postBody: post.postBody });
+    }
+  });
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
