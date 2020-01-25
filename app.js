1a9e9d5a0ddbc192b2e920d0e2a627481f968c00
RishabMunot <rishabmunot.az@gmail.com>
2020-01-25 16:38:59 +0530

    challenge 16 completed

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+const posts = []
+
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -19,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.render("home", { content: homeStartingContent });
+  res.render("home", { content: homeStartingContent,posts:posts})
 });
 
 app.get("/about", function(req, res) {
@@ -35,11 +37,18 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
- 
+  
   const post = { postTitle: req.body.postTitle, postBody: req.body.postBody };
-  res.redirect("/compose");
+  posts.push(post)
+  
+  res.redirect("/");
 
 });
+
+
+app.get("/posts/:postName",function (req,res) {
+  console.log(req.params.postName); 
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
